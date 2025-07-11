a23d022766ca45f01f3388a8f08cb6782dc749c8
Glen Hunter <29704413+ghhunter@users.noreply.github.com>
2025-07-11 16:08:21 +0200

    Fix: gpu flag not being read by pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,7 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption('--gpu',action='store_true',dest="gpu",
                      default=False, help="enable gpu memory release tests")
+    
+@pytest.fixture
+def gpu(request):
+    return request.config.getoption("--gpu")
